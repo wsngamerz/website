7d56079a4e2925ee6ada0274b33744032b7bd75a
William Neild <wneild5@gmail.com>
2021-04-03 11:08:57 +0100

    Clean the workspace

--- src/components/YouTube.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-import YouTubeEmbed from "react-youtube"
-
-const YouTube = ({ videoId }) => {
-    return <YouTubeEmbed containerClassName="my-2 youtube-container" className="rounded" videoId={videoId} />
-}
-
-export default YouTube
