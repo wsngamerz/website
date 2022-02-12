ead65b7182dd4dd759dcf2626a1715932653dd5e
William Neild <wneild5@gmail.com>
2022-02-12 00:21:31 +0000

    fresh install of gatsby v4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,99 +1,43 @@
 module.exports = {
     siteMetadata: {
-        title: "William Neild",
-        titleTemplate: "%s · William Neild",
-        description: "An aspiring student who loves software development",
-        alternativeDescriptions: [
-            "A student who adores technology",
-            "A future blogging star (or so he thinks)",
-            "A passionate nerd",
-        ],
-        author: "William Neild",
-        siteUrl: "https://williamneild.com",
-        image: "",
-        socialMedia: {
-            twitterUrl: "https://twitter.com/wsngamerz",
-            twitterUsername: "wsngamerz",
-            facebookUrl: "https://www.facebook.com/william.neild.2",
-            githubUrl: "https://github.com/wsngamerz",
-            instagramUrl: "https://www.instagram.com/wneild5",
-            linkedinUrl: "https://www.linkedin.com/in/wneild5",
-        },
+        title: `William Neild`,
+        siteUrl: `https://williamneild.com`,
     },
     plugins: [
-        "gatsby-plugin-offline",
-        "gatsby-plugin-react-helmet",
         "gatsby-plugin-sass",
-        // "gatsby-remark-reading-time",
-        "gatsby-plugin-image",
         {
-            resolve: "gatsby-transformer-sharp",
+            resolve: "gatsby-plugin-google-analytics",
             options: {
-                checkSupportedExtensions: false,
+                trackingId: "UA-75997847-9",
             },
         },
+        "gatsby-plugin-image",
+        "gatsby-plugin-react-helmet",
+        "gatsby-plugin-sitemap",
         {
-            resolve: "gatsby-plugin-sharp",
+            resolve: "gatsby-plugin-manifest",
             options: {
-                stripMetadata: true,
-                defaultQuality: 90,
-                useMozJpeg: true,
+                icon: "src/images/icon.png",
             },
+        },
+        "gatsby-plugin-mdx",
+        "gatsby-plugin-sharp",
+        "gatsby-transformer-sharp",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "images",
+                path: "./src/images/",
+            },
+            __key: "images",
         },
         {
             resolve: "gatsby-source-filesystem",
             options: {
-                path: `${__dirname}/content`,
+                name: "pages",
+                path: "./src/pages/",
             },
-        },
-        {
-            resolve: "gatsby-plugin-sentry",
-            options: {
-                dsn: "https://a8e3a683c5f84b9cbd1928e1145c1dc5@o294554.ingest.sentry.io/5249583",
-                environment: process.env.NODE_ENV,
-                enabled: (() => ["production", "stage", "development"].indexOf(process.env.NODE_ENV) !== -1)(),
-            },
-        },
-        {
-            resolve: "gatsby-plugin-mdx",
-            options: {
-                defaultLayouts: {
-                    posts: require.resolve("./src/templates/blogTemplate.js"),
-                    projects: require.resolve("./src/templates/projectTemplate.js"),
-                },
-                gatsbyRemarkPlugins: [
-                    {
-                        resolve: "gatsby-remark-images",
-                        options: {
-                            maxWidth: 1200,
-                        },
-                    },
-                ],
-            },
-        },
-        {
-            resolve: "gatsby-plugin-advanced-sitemap",
-            options: {
-                createLinkInHead: true,
-            },
-        },
-        {
-            resolve: "gatsby-plugin-manifest",
-            options: {
-                name: "William Neild",
-                short_name: "William Neild",
-                start_url: "/",
-                background_color: "#663399",
-                theme_color: "#663399",
-                display: "minimal-ui",
-                icon: "content/images/wn-icon.png",
-            },
-        },
-        {
-            resolve: `gatsby-plugin-gtag`,
-            options: {
-                trackingId: "UA-75997847-9",
-            },
+            __key: "pages",
         },
     ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,196 +1,183 @@
-import React from "react";
+import * as React from "react";
 
-import { Link, graphql } from "gatsby";
-import { FaFacebook, FaInstagram, FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+// styles
+const pageStyles = {
+    color: "#232129",
+    padding: 96,
+    fontFamily: "-apple-system, Roboto, sans-serif, serif",
+};
+const headingStyles = {
+    marginTop: 0,
+    marginBottom: 64,
+    maxWidth: 320,
+};
+const headingAccentStyles = {
+    color: "#663399",
+};
+const paragraphStyles = {
+    marginBottom: 48,
+};
+const codeStyles = {
+    color: "#8A6534",
+    padding: 4,
+    backgroundColor: "#FFF4DB",
+    fontSize: "1.25rem",
+    borderRadius: 4,
+};
+const listStyles = {
+    marginBottom: 96,
+    paddingLeft: 0,
+};
+const listItemStyles = {
+    fontWeight: 300,
+    fontSize: 24,
+    maxWidth: 560,
+    marginBottom: 30,
+};
 
-import Layout from "../components/Layout";
-import Meta from "../components/Meta";
-import PageHeader from "../components/PageHeader";
-import Card from "../components/Card";
-import SocialMediaItem from "../components/SocialMediaItem";
-import Advert from "../components/Advert";
+const linkStyle = {
+    color: "#8954A8",
+    fontWeight: "bold",
+    fontSize: 16,
+    verticalAlign: "5%",
+};
 
-const IndexPage = ({ data }) => {
+const docLinkStyle = {
+    ...linkStyle,
+    listStyleType: "none",
+    marginBottom: 24,
+};
+
+const descriptionStyle = {
+    color: "#232129",
+    fontSize: 14,
+    marginTop: 10,
+    marginBottom: 0,
+    lineHeight: 1.25,
+};
+
+const docLink = {
+    text: "Documentation",
+    url: "https://www.gatsbyjs.com/docs/",
+    color: "#8954A8",
+};
+
+const badgeStyle = {
+    color: "#fff",
+    backgroundColor: "#088413",
+    border: "1px solid #088413",
+    fontSize: 11,
+    fontWeight: "bold",
+    letterSpacing: 1,
+    borderRadius: 4,
+    padding: "4px 6px",
+    display: "inline-block",
+    position: "relative",
+    top: -2,
+    marginLeft: 10,
+    lineHeight: 1,
+};
+
+// data
+const links = [
+    {
+        text: "Tutorial",
+        url: "https://www.gatsbyjs.com/docs/tutorial/",
+        description:
+            "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
+        color: "#E95800",
+    },
+    {
+        text: "How to Guides",
+        url: "https://www.gatsbyjs.com/docs/how-to/",
+        description:
+            "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
+        color: "#1099A8",
+    },
+    {
+        text: "Reference Guides",
+        url: "https://www.gatsbyjs.com/docs/reference/",
+        description:
+            "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
+        color: "#BC027F",
+    },
+    {
+        text: "Conceptual Guides",
+        url: "https://www.gatsbyjs.com/docs/conceptual/",
+        description:
+            "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
+        color: "#0D96F2",
+    },
+    {
+        text: "Plugin Library",
+        url: "https://www.gatsbyjs.com/plugins",
+        description:
+            "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
+        color: "#8EB814",
+    },
+    {
+        text: "Build and Host",
+        url: "https://www.gatsbyjs.com/cloud",
+        badge: true,
+        description:
+            "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
+        color: "#663399",
+    },
+];
+
+// markup
+const IndexPage = () => {
     return (
-        <Layout>
-            <Meta title="Home" />
-            <PageHeader
-                title={data.site.siteMetadata.title}
-                subtitle={data.site.siteMetadata.description}
-                typewriter={true}
-                alternatives={data.site.siteMetadata.alternativeDescriptions}
-                big={true}
-                buttons={[
-                    ["Blog", "/blog"],
-                    ["Projects", "/projects"],
-                ]}
+        <main style={pageStyles}>
+            <title>Home Page</title>
+            <h1 style={headingStyles}>
+                Congratulations
+                <br />
+                <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
+                <span role="img" aria-label="Party popper emojis">
+                    🎉🎉🎉
+                </span>
+            </h1>
+            <p style={paragraphStyles}>
+                Edit <code style={codeStyles}>src/pages/index.js</code> to see this page update in real-time.{" "}
+                <span role="img" aria-label="Sunglasses smiley emoji">
+                    😎
+                </span>
+            </p>
+            <ul style={listStyles}>
+                <li style={docLinkStyle}>
+                    <a
+                        style={linkStyle}
+                        href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
+                    >
+                        {docLink.text}
+                    </a>
+                </li>
+                {links.map(link => (
+                    <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
+                        <span>
+                            <a
+                                style={linkStyle}
+                                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
+                            >
+                                {link.text}
+                            </a>
+                            {link.badge && (
+                                <span style={badgeStyle} aria-label="New Badge">
+                                    NEW!
+                                </span>
+                            )}
+                            <p style={descriptionStyle}>{link.description}</p>
+                        </span>
+                    </li>
+                ))}
+            </ul>
+            <img
+                alt="Gatsby G Logo"
+                src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
             />
-            <div className="bg-dark text-light">
-                <div className="container py-4">
-                    <div className="row py-4">
-                        <div className="col-12">
-                            <h3>Hello and welcome to my website</h3>
-                            <p>
-                                Here you will be able to view a wonderful selection of my projects and have a read
-                                through some of my blog posts where I will talk about a variety of topics including
-                                software development, recent technological advancements and also some of my own life
-                                stories (because people want to know about that).
-                            </p>
-                            <p>I hope that you enjoy your time here!</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div className="">
-                <div className="container pt-4">
-                    <div className="row pt-4">
-                        <div className="col-12">
-                            <h3>Projects</h3>
-                            <p>Here is a list of my projects, basically my portfolio.</p>
-                            <div className="card-group py-1">
-                                {data.latestProjects.edges.map(edge => {
-                                    const project = edge.node.frontmatter;
-                                    return (
-                                        <Card
-                                            key={project.slug}
-                                            title={project.title}
-                                            thumbnail={project.thumbnail.childImageSharp.gatsbyImageData}
-                                            description={project.description}
-                                            url={project.slug}
-                                        />
-                                    );
-                                })}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div>
-                <div className="container">
-                    <div className="row">
-                        <div className="col-12 my-4">
-                            <Advert />
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div className="bg-light">
-                <div className="container py-4">
-                    <div className="row py-4">
-                        <div className="col-12">
-                            <h3 className="mb-3">Recent Posts</h3>
-                        </div>
-                        <div className="col-sm-12 col-md-8">
-                            {data.latestPosts.edges
-                                .filter(p => p.node.frontmatter.publish !== false)
-                                .map(edge => {
-                                    const post = edge.node.frontmatter;
-                                    return (
-                                        <div className="col-12 mb-4 p-0" key={post.slug}>
-                                            <Card
-                                                title={post.title}
-                                                thumbnail={post.thumbnail.childImageSharp.gatsbyImageData}
-                                                description={edge.node.excerpt}
-                                                url={post.slug}
-                                                post={{
-                                                    author: post.author,
-                                                    date: post.date,
-                                                }}
-                                            />
-                                        </div>
-                                    );
-                                })}
-                            <div className="col-12 p-0">
-                                <Link className="btn btn-secondary btn-block mb-4" to="/blog">
-                                    View more
-                                </Link>
-                            </div>
-                        </div>
-
-                        <div className="col-sm-12 col-md-4">
-                            <div className="list-group">
-                                <div className="list-group-item list-group-item-secondary">
-                                    <p className="m-0">Social Media</p>
-                                </div>
-                                <SocialMediaItem
-                                    text="Facebook"
-                                    icon={FaFacebook}
-                                    url="https://www.facebook.com/william.neild.2"
-                                />
-                                <SocialMediaItem text="Twitter" icon={FaTwitter} url="https://twitter.com/wsngamerz" />
-                                <SocialMediaItem
-                                    text="Instagram"
-                                    icon={FaInstagram}
-                                    url="https://instagram.com/wneild5"
-                                />
-                                <SocialMediaItem text="Github" icon={FaGithub} url="https://github.com/wsngamerz" />
-                                <SocialMediaItem
-                                    text="Linkedin"
-                                    icon={FaLinkedin}
-                                    url="https://linkedin.com/in/wneild5"
-                                />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </Layout>
+        </main>
     );
 };
 
 export default IndexPage;
-
-export const query = graphql`
-    query HomePageQuery {
-        site {
-            siteMetadata {
-                title
-                description
-                alternativeDescriptions
-            }
-        }
-        latestPosts: allMdx(
-            filter: { frontmatter: { slug: { regex: "/blog/" } } }
-            sort: { order: DESC, fields: [frontmatter___date] }
-            limit: 5
-        ) {
-            edges {
-                node {
-                    excerpt(pruneLength: 160)
-                    frontmatter {
-                        date
-                        slug
-                        title
-                        description
-                        author
-                        thumbnail {
-                            childImageSharp {
-                                gatsbyImageData
-                            }
-                        }
-                        publish
-                    }
-                }
-            }
-        }
-        latestProjects: allMdx(filter: { frontmatter: { slug: { regex: "/projects/" } } }, limit: 5) {
-            edges {
-                node {
-                    frontmatter {
-                        slug
-                        title
-                        description
-                        thumbnail {
-                            childImageSharp {
-                                gatsbyImageData
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-`;
